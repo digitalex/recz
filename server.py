16eb274ad3a45e194a395725d3066d9cad9839c2
Alexander Mossin <alexander@companybook.no>
2012-03-12 10:02:39 +0100

    Added timers

--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 import fileinput
 import tornado.web
 import tornado.ioloop
+from time import clock
 from collections import defaultdict
 
 PORT = 8888
@@ -30,6 +31,7 @@ class Recz:
         sessionset = sessions.pop(item_id, set())
         itemcounts = defaultdict(int)
 
+        # Count the occurrences of other items viewed by the same sessions
         for session_id in sessionset:
             itemset = self.items.get(session_id, set())
             itemset.discard(item_id)
@@ -50,19 +52,27 @@ class Server(tornado.web.RequestHandler):
     def get(self):
         item_id = self.get_argument('itemId')
         k = int(self.get_argument('k', 5))
+        start_recommend = clock()
         self.write({'itemIds': self.recz.recommend(item_id, k)})
+        print "Recommended %d items for %s in %f seconds." % 
+              (k, item_id, clock() - start_recommend)
 
 
 if __name__ == "__main__":
     recz = Recz()
 
+    i = 0
+    start_read = clock()
     for line in fileinput.input():
       vals = line.split()
       if len(vals) == 2:
         item_id, session_id = vals
         recz.add_example(item_id, session_id)
+        i += 1
 
     recz.compact()
+    print "Processed %d examples in %f seconds." % (i, clock() - start_read)
+
     app = tornado.web.Application([(r'/recommend', Server, dict(recz=recz)),])
     app.listen(PORT)
     print "Server ready and listening at port %d!" % PORT
